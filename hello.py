404777949a8e5dafe84446717d888b5b30916f01
aakuichoue <yakuichoue@gmail.com>
2021-03-18 14:31:15 -0400

    initial config

---
+++ hello.py
@@ -0,0 +1,11 @@
+import sys
+
+print(sys.version)
+print(sys.executable)
+print("Hello World")
+print ("Hello W00orld")
+x = 2
+if x == 4:
+    print("Hello World 2")
+else:
+    print("You lost rire")
